b80f2f4cdbb32134a3bbc375f0a87c1f7280a5da
Aya-Khaled562 <77298939+Aya-Khaled562@users.noreply.github.com>
2023-08-07 10:45:05 +0300

    ..

--- Js v2/Forms/preview_form.js
+++ Js v2/Forms/preview_form.js
@@ -7,20 +7,12 @@ export default class PreviewFrom {
     }
 
     initialize(){
-        // console.log("Initializing in Preview")
         const jsonData = JSON.parse(localStorage.getItem('jsonDataForm'));
-        // console.log('prevew dasta >>', jsonData);
         this.#builder = new FormBuilder(jsonData, 'preview', 'form');
-
         let copyHtmlBtn = document.getElementById('copyFormHtml');
         copyHtmlBtn.addEventListener('click', this.handleCopyHtml(this));
-
-        // let editFromBtn = document.getElementById('EditForm');
-        // editFromBtn.addEventListener('click',this.handleEditForm );
-        // console.log('builder', this.#builder)
     }
 
-    
     handleCopyHtml(prev){
         return function fun (e){
             let formContainer = document.getElementById(prev.#builder.ParentId).parentElement;
@@ -28,10 +20,9 @@ export default class PreviewFrom {
         }
     }
    
-    
     handleEditForm(e){
         sessionStorage.setItem('formMode', 'update');
-        window.open('/Form-Builder/index.html', '_self');
+        window.open('/index.html', '_self');
     }
 
 }
