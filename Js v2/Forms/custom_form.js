4c7aa3d6b93286b72929badf5edee0e497d72b82
Aya-Khaled562 <77298939+Aya-Khaled562@users.noreply.github.com>
2023-08-15 15:58:24 +0300

    add validatoin on save

--- Js v2/Forms/custom_form.js
+++ Js v2/Forms/custom_form.js
@@ -3,18 +3,18 @@ import FormBuilder from "../formbuilder.js";
 
 export default class CustomForm {
     builder;
-    values;
     resolvePromise;
     targetData;
     flag;
     targetId;
+    requiredFields
     constructor(){
         this.builder = null;
-        this.values = [];
         this.resolvePromise = null;
         this.targetData = null;
         this.flag = false;
         this.targetId = 0;
+        this.requiredFields = [];
     }
 
     // initialize(){
@@ -34,6 +34,9 @@ export default class CustomForm {
         const form = await this.getForm();
         const mainForm = form[0];
         const formAfterParse = JSON.parse(mainForm.fromJson);
+        // console.log('form: ', formAfterParse);
+        this.setRequiredFields(formAfterParse)
+        console.log('required fields: ', this.requiredFields);
         //Get Data
         this.targetData = JSON.parse(localStorage.getItem('targetData'));
         this.builder = new FormBuilder(formAfterParse, 'custom' ,'form');
@@ -43,18 +46,6 @@ export default class CustomForm {
         let removeBtn = document.getElementById('removeBtn');
         let newBtn = document.getElementById('new');
         newBtn.addEventListener('click' ,this.handleNewBtn);
-        
-        // if(this.targetData !== null){
-        //     this.builder.mapData(this.targetData);
-        //     console.log('Updated');
-        //     saveBtn.addEventListener('click', ()=> this.handleNewSaveBtn('PUT', false,this.targetData.id))
-        //     saveandcloseBtn.addEventListener('click',()=> this.handleNewSaveBtn('PUT', true, this.targetData.id))
-        //     
-        // }else{
-        //     console.log('Add');
-        //     saveBtn.addEventListener('click',()=> this.handleNewSaveBtn('POST', false));
-        //     saveandcloseBtn.addEventListener('click',()=> this.handleNewSaveBtn('POST', true));
-        // }
 
         if(this.targetData !== null){
             this.builder.mapData(this.targetData);
@@ -66,74 +57,67 @@ export default class CustomForm {
     
     }
 
+    setRequiredFields(form){
+        form.elements.forEach(tab => {
+            tab.elements.forEach(coltab=>{
+                coltab.elements.forEach(sec=>{
+                    sec.elements.forEach(colsec => {
+                        colsec.elements.forEach(field => {
+                            if(field.isRequired){
+                                this.requiredFields.push(field);
+                            }
+                        })
+                    })
+                })
+            })
+        });
+    }
     async getForm(){
         const form = await fetch('http://localhost:5032/api/EntityFroms?formName=main');
         return form.json();
     }
-    
-    get Values(){
-        return this.values;
-    }
-
-    // handleNewSaveBtn(param, method,shouldClose = false,id=''){
-    //     return async function handler(e){
-    //         let dataObject = {}
-    //         for(let i=0; i< param.builder.Fields.length; i++){
-    //             let key = param.builder.Fields[i].name;
-    //             let value = document.getElementById(param.builder.Fields[i].id).value
-    //             dataObject[key]  = value;
-    //         }
-    //         try{
-    //             // this.flag = true;
-    //             await param.pushDataIntoDB(dataObject , method , id);
-    //             console.log('window opener:', window.opener);
-    //             if (shouldClose) {
-    //                 window.close();
-    //                 window.location.reload();
-    //                 window.history.back();
-    //             }
-    //         }catch(error){
-    //             console.error('Error pushing data into DB:', error);
-    //         }
-            
-    //     }
-    // }
 
     async handleNewSaveBtn(shouldClose){
         let dataObject = {}
+        let flag = false;
         for(let i=0; i< this.builder.Fields.length; i++){
             let key = this.builder.Fields[i].name;
             let value = document.getElementById(this.builder.Fields[i].id).value
             dataObject[key]  = value;
         }
 
+        this.requiredFields.forEach(field => {
+            let requiredField = dataObject.hasOwnProperty(field.name);
+            if(requiredField){
+                console.log('field ', dataObject[field.name]);
+                if(!dataObject[field.name]){
+                    flag = true;
+                }
+            }
+        });
+
+        if(flag){
+            alert('Please Enter the required Data?');
+            return;
+        }
+
         try{
             let response = null;
             let dataFromLocalStorage = localStorage.getItem('newRecordFlag');
             let data = JSON.parse(dataFromLocalStorage);
-            console.log('data', data);
-            console.log('dataFromLocalStorage:', dataFromLocalStorage);
             if(this.targetData !== null || data !== null){
-                console.log('update');
                 this.targetId = this.targetData?.id || data?.id;
-                console.log('id in localStorage: ' , this.targetId);
+                // console.log('id in localStorage: ' , this.targetId);
                 response = await this.pushDataIntoDB(dataObject , 'PUT' , this.targetId);
             }
             else{
-                console.log('Add')
                 response = await this.pushDataIntoDB(dataObject , 'POST');
-                localStorage.setItem('newRecordFlag', JSON.stringify(response))
-                console.log('responsesdf', response);
+                localStorage.setItem('newRecordFlag', JSON.stringify(response));
             }
-            // console.log('response', response);
-            console.log('window.opener' , window.opener);
             if (shouldClose) {
                 localStorage.setItem('targetData', null);
                 localStorage.setItem('newRecordFlag',null);
-                
-                // window.close();
                 window.open('../../pages/showSavedRecord.html' , '_self');
-                // window.history.back();
             }
         }catch(error){
             console.error('Error pushing data into DB:', error);
@@ -166,7 +150,6 @@ export default class CustomForm {
     }
 
     async pushDataIntoDB(data , method , id=''){
-        console.log('id', id);
         data.departmentId = 1;
         console.log('data: ', data);
         const response = await fetch(`http://localhost:5032/api/Employees/${id}`,{
